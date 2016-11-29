21d4438cb058cd7d90bcf89876018cd6b28349ed
golden-bear-12 <m.honrychs@gmail.com>
2016-11-29 01:17:53 -0500

    file names fixed, heroku deployed, email confirmation, flash messages, update profile redirect altered, root page issue resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => :registrations}
 
   resources :items
 
   resources :users, only: [:show]
 
-  root "home#index"
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
