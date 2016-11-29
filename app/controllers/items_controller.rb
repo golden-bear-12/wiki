095711f52c7b72ecf8136ebd43cce5bca2e01c64
golden-bear-12 <m.honrychs@gmail.com>
2016-11-29 15:29:26 -0500

    CRUD actions + partials, layout links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,57 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = current_user.items.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Wiki was saved successfully."
+      redirect_to @item
+    else
+      flash.now[:alert] = "There was an error saving the item. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update_attributes(item_params)
+     flash[:notice] = "Wiki was updated successfully."
+      redirect_to @item
+    else
+     flash.now[:alert] = "There was an error saving the item. Please try again."
+     render :edit
+    end
   end
 
   def destroy
-  end
+    @item = Item.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "\"#{@item.title}\" was deleted successfully."
+      redirect_to items_path
+    else
+      flash.now[:alert] = "There was an error deleting the item."
+      render :show
+    end
+   end
+
+   private
+   def item_params
+    params.require(:item).permit(:title, :body)
+   end
+
 end
--- db/migrate/20161129190031_create_items.rb
+++ db/migrate/20161129190031_create_items.rb
@@ -1,6 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
+      t.string :title
+      t.text :body
+      t.boolean :private
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
